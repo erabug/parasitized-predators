a2a80f7d6b76df9f6ee464c08394b8848dcb1f64
erabug <erabug@gmail.com>
2014-09-04 19:35:46 -0500

    Fixed the contour plot function to expand parameter ranges.

--- vigilanceModel.R
+++ vigilanceModel.R
@@ -6,23 +6,23 @@ library(reshape2)
 #---------------------------------------#
 
 # Model parameters and values
-parameters <- c(r = 0.2,                     #intrinsic rate of growth
-                v = 0.105,                   #vigilance
-                dN = 0.1,                    #prey death rate
-                K = 100,                     #carrying capacity
-                c = 0.1,                     #conversion rate
-                dP = 0.1,                    #predator death rate
-                m = 0.1,                     #encounter rate
-                k = 1,                       #inverse of predator lethality
-                b = 5,                       #value of vigilance
-                z = expression(m/(k+(b*v))), #predation risk
-                r0 = expression((1-v)*r))    #prey birth rate 
+parameters <- c(r = 0.2,                     # intrinsic rate of growth
+                v = 0.105,                   # vigilance
+                dN = 0.1,                    # prey death rate
+                K = 100,                     # carrying capacity
+                c = 0.1,                     # conversion rate
+                dP = 0.1,                    # predator death rate
+                m = 0.1,                     # encounter rate
+                k = 1,                       # inverse of predator lethality
+                b = 5,                       # value of vigilance
+                z = expression(m/(k+(b*v))), # predation risk
+                r0 = expression((1-v)*r))    # prey birth rate 
 
 
 # Initial conditions
-max <- with(as.list(parameters), return(K*(1-(dN/r)))) #maximum starting N & isocline range
-state <- c(N = max, P = 5)                             #starting population sizes
-times <- expression(seq(0, 1000, by = 1))              #number of iterations to run
+max <- with(as.list(parameters), return(K*(1-(dN/r)))) # maximum starting N & isocline range
+state <- c(N = max, P = 5)                             # starting population sizes
+times <- expression(seq(0, 1000, by = 1))              # number of iterations to run
 
 # Isocline range
 isoRange <- expression(seq(0, max, 1))
@@ -41,7 +41,10 @@ plotSweepV(sweep)
 
 # G-functions
 Gvalues <- getGP(state, times, parameters)
-contourPlot(Gvalues, "m", "b") #choose m+b, k+b, or k+m
+contourPlot(Gvalues, "k", "b") # choose m+b, k+b, or k+m
+points(.3, 0.1, cex = 1.5, pch = 19) # make a dot on the k+m plot (600x500 image size)
+points(.03, 5, cex = 1.5, pch = 19) # make a dot on the m+b plot (600x500 image size)
+points(.3, 5, cex = 1.5, pch = 19) # make a dot on the k+b plot (600x500 image size)
 
 #---------------------------------------#
 #              Functions                #
@@ -52,10 +55,10 @@ contourPlot(Gvalues, "m", "b") #choose m+b, k+b, or k+m
 # with fixed vigilance, calculates change in N and P over a time step
 fixedDynamics <- function(t, state, parameters) {
     with(as.list(c(state, parameters)),{
-        z <- eval(z)                           #evaluate z
-        r0 <- eval(r0)                         #evaluate r0
-        dN.dt <- r0*N*((K-N)/K)-(dN*N)-(z*N*P) #determine change in N
-        dP.dt <- ((z*c*N)-dP)*P                #determine change in P
+        z <- eval(z)                           # evaluate z
+        r0 <- eval(r0)                         # evaluate r0
+        dN.dt <- r0*N*((K-N)/K)-(dN*N)-(z*N*P) # determine change in N
+        dP.dt <- ((z*c*N)-dP)*P                # determine change in P
         list(c(dN.dt, dP.dt))      
     })
 }
@@ -63,12 +66,12 @@ fixedDynamics <- function(t, state, parameters) {
 # with flexible vigilance, calculates change in N and P over a time step
 flexibleDynamics <- function(t, state, parameters) {
     with(as.list(c(state, parameters)),{   
-        v <- (((m*K*P)/(r*b*(K-N)))^0.5)-(k/b) #determine vigilance, overwrite value in parameters
-        if (v < 0 | v > 1 | is.na(v)) v <- 0   #if v is less than 0, or greater than 1, set it to 0
-        z <- eval(z)                           #evaluate z
-        r0 <- eval(r0)                         #evaluate r0
-        dN.dt <- r0*N*((K-N)/K)-(dN*N)-(z*N*P) #determine change in N
-        dP.dt <- ((z*c*N)-dP)*P                #determine change in P
+        v <- (((m*K*P)/(r*b*(K-N)))^0.5)-(k/b) # determine vigilance, overwrite value in parameters
+        if (v < 0 | v > 1 | is.na(v)) v <- 0   # if v is less than 0, or greater than 1, set it to 0
+        z <- eval(z)                           # evaluate z
+        r0 <- eval(r0)                         # evaluate r0
+        dN.dt <- r0*N*((K-N)/K)-(dN*N)-(z*N*P) # determine change in N
+        dP.dt <- ((z*c*N)-dP)*P                # determine change in P
         list(c(dN.dt, dP.dt))                  
     })
 } 
@@ -81,7 +84,7 @@ getDynamics <- function(state, times, parameters, modelType) {
             v <- (((m*K*dynamics[,"P"])/(r*b*(K-dynamics[,"N"])))^0.5)-(k/b)
             v[v < 0 | is.na(v)] <- 0
         }
-        return(data.frame(dynamics, v)) #this just tacks on v to the output, can't find a better way to do this
+        return(data.frame(dynamics, v)) # this just tacks on v to the output, can't find a better way to do this
     })
 }
 
@@ -110,14 +113,14 @@ predEquilibrium <- function(parameters, vigilance) {
 # return data frame of N and P isocline values (fixed vigilance)
 fixedIsoclines <- function(isoRange, parameters) {
     with(as.list(parameters),{
-        z <- eval(z)                                     #evaluate z
-        r0 <- eval(r0)                                   #evaluate r0
-        N <- eval(isoRange)                              #find the range of N values
-        P <- (r0-dN)/z - (r0*N)/(z*K)                    #find the prey isocline value (P) for each N
-        Niso <- data.frame(N = N, P = P, isocline = "N") #write N and P to a dataframe    
-        P <- 0.25*N                                      #set the range of P values to 25% N
-        N <- dP/(c*z)                                    #find the predator isocline value (N) for each P
-        Piso <- data.frame(N = N, P = P, isocline = "P") #write N and P to a dataframe    
+        z <- eval(z)                                     # evaluate z
+        r0 <- eval(r0)                                   # evaluate r0
+        N <- eval(isoRange)                              # find the range of N values
+        P <- (r0-dN)/z - (r0*N)/(z*K)                    # find the prey isocline value (P) for each N
+        Niso <- data.frame(N = N, P = P, isocline = "N") # write N and P to a dataframe    
+        P <- 0.25*N                                      # set the range of P values to 25% N
+        N <- dP/(c*z)                                    # find the predator isocline value (N) for each P
+        Piso <- data.frame(N = N, P = P, isocline = "P") # write N and P to a dataframe    
         return(rbind(Niso, Piso))
     })
 }
@@ -125,17 +128,17 @@ fixedIsoclines <- function(isoRange, parameters) {
 # return data frame of N and P isocline values (flexible vigilance)
 flexibleIsoclines <- function(isoRange, parameters) {
     with(as.list(parameters),{
-        N <- eval(isoRange)                                       #evaluate the range of N to test
-        P <- b*K*((r*(1+k/b)*((K-N)/K)-dN)^2)/(4*r*m*(K-N))       #evaluate prey isocline (P) for N range (Abdel's version)
-        #P <- ((K*b*dN-K*b*r-K*k*r+N*b*r+N*k*r)^2)/(4*b*r*K*m*(K-N)) #Paul's version (same result as Abdel's)
-        v <- (((m*K*P)/(r*b*(K-N)))^0.5)-(k/b)                    #evaluate v for those N and P values
-        Niso <- data.frame(N = N, P = P, v = v, isocline = "N")   #write N, P, v to a data frame
-        Niso[Niso$v < 0, "P"] <- (k*(r-dN))/m - ((r*k*Niso[Niso$v < 0, "N"])/(m*K)) #if v negative, recalculate isocline (P)
-        P <- (((c*N)/dP)^2)*(r*m*(K-N)/(b*K))                     #evaluate predator isocline (P) for N range 
-        v <- (((m*K*P)/(r*b*(K-N)))^0.5)-(k/b)                    #evaluate v for the new N and P values
-        Piso <- data.frame(N = N, P = P, v = v, isocline = "P")   #write N, P, v to a data frame
-        Piso[Piso$v < 0, "N"] <- (k*dP)/(c*m)                     #if v negative, recalculate isocline (N)
-        P <- (r*k^2)/(b*m) - (r*k^2*N)/(b*m*K)                    #plot the isoleg where v is 0
+        N <- eval(isoRange)                                       # evaluate the range of N to test
+        P <- b*K*((r*(1+k/b)*((K-N)/K)-dN)^2)/(4*r*m*(K-N))       # evaluate prey isocline (P) for N range (Abdel's version)
+        #P <- ((K*b*dN-K*b*r-K*k*r+N*b*r+N*k*r)^2)/(4*b*r*K*m*(K-N)) # Paul's version (same result as Abdel's)
+        v <- (((m*K*P)/(r*b*(K-N)))^0.5)-(k/b)                    # evaluate v for those N and P values
+        Niso <- data.frame(N = N, P = P, v = v, isocline = "N")   # write N, P, v to a data frame
+        Niso[Niso$v < 0, "P"] <- (k*(r-dN))/m - ((r*k*Niso[Niso$v < 0, "N"])/(m*K)) # if v negative, recalculate isocline (P)
+        P <- (((c*N)/dP)^2)*(r*m*(K-N)/(b*K))                     # evaluate predator isocline (P) for N range 
+        v <- (((m*K*P)/(r*b*(K-N)))^0.5)-(k/b)                    # evaluate v for the new N and P values
+        Piso <- data.frame(N = N, P = P, v = v, isocline = "P")   # write N, P, v to a data frame
+        Piso[Piso$v < 0, "N"] <- (k*dP)/(c*m)                     # if v negative, recalculate isocline (N)
+        P <- (r*k^2)/(b*m) - (r*k^2*N)/(b*m*K)                    # plot the isoleg where v is 0
         isoLeg <- data.frame(N = N, P = P, v = 0, isocline = "leg")
         return(rbind(Niso, Piso, isoLeg))
     })
@@ -243,8 +246,8 @@ plotSweepV <- function(sweep) {
 # return data frame of G-function values for range in m, k, and b
 getGP <- function(state, times, parameters){
     last <- length(eval(times))   
-    mRange <- seq(0, 0.50, 0.05)
-    kRange <- bRange <- range <- seq(1, 10, 1)    
+    mRange <- seq(0, 1, 0.1)
+    kRange <- bRange <- range <- seq(0, 10, 1)    
     Gvalues <- data.frame()
     vals <- getDynamics(state, times, parameters, "flexibleDynamics")[last,]
     N <- vals[,2]
@@ -312,12 +315,14 @@ sweepGvalues <- function(parameters) {
 
 # create a filled contour plot of G-function values for pairing of m, k, and b
 contourPlot <- function(Gvalues, xpar, ypar) {
-    xlim <- ylim <- 1:10
+    xlim <- ylim <- 0:10
     par <- c(xpar, ypar)
-    ifelse(xpar == "m", {xlim <- unique(Gvalues$m); xlab <- "m"}, 
-           ifelse(xpar == "k", xlab <- "k", xlab <- "b"))
-    ifelse(ypar == "m", {ylim <- unique(Gvalues$m); ylab <- "m"}, 
-           ifelse(ypar == "k", ylab <- "k", ylab <- "b"))
+    ifelse(xpar == "m", {xlim <- unique(Gvalues$m); xlab <- "encounter rate (m)"}, 
+           ifelse(xpar == "k", xlab <- "inverse predator lethality (k)", 
+                  xlab <- "value of vigilance (b)"))
+    ifelse(ypar == "m", {ylim <- unique(Gvalues$m); ylab <- "encounter rate (m)"}, 
+           ifelse(ypar == "k", ylab <- "inverse predator lethality (k)", 
+                  ylab <- "value of vigilance (b)"))
     ifelse(!("m" %in% par), exclude <- "m", 
            ifelse(!("k" %in% par), exclude <- "k", exclude <- "b"))
     ifelse(exclude == "k",
@@ -325,11 +330,11 @@ contourPlot <- function(Gvalues, xpar, ypar) {
            ifelse(exclude == "m",
                   Y <- acast(Gvalues[Gvalues$m == 0.1, -1], k ~ b),
                   Y <- acast(Gvalues[Gvalues$b == 5, -3], k ~ m)))
-    return(filled.contour(xlim, ylim, Y, color.palette = heat.colors, nlevels = 3,
-                          xlab = xlab, ylab = ylab, cex.lab = 1.5, cex.main = 3,
+    return(filled.contour(xlim, ylim, Y, color.palette = heat.colors, nlevels = 4,
+                          xlab = xlab, ylab = ylab, cex.lab = 1.5, #cex.main = 3,
                           #main = paste("predator G-value with change in", xpar, "and", ypar),  
                           plot.axes={ axis(1, xlim); axis(2, ylim); 
                                       contour(x = xlim, y = ylim, z = Y, 
                                               drawlabels = FALSE, add = T,
-                                              nlevels = 3) }))
+                                              nlevels = 4) }))
 }
